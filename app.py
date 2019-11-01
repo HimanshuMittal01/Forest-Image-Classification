0ace3f9c6c52622ac1322e6edd4be150e1ca5359
HimanshuMittal01 <44058642+HimanshuMittal01@users.noreply.github.com>
2019-11-01 21:21:44 +0530

    Added frontend

--- app.py
+++ app.py
@@ -66,8 +66,24 @@ def predictClass(img_array):
     print(labels)
     return labels
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            session['filename'] = filename
+            return redirect(url_for('predict'))
     return render_template('home.html')
 
 @app.route('/stats')
